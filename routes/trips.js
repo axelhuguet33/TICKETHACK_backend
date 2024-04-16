c254eb329793ed77b6b07d57465a9c6e351b39ba
Your name <Your email>
2024-04-16 15:07:40 +0200

    work en trips

--- routes/trips.js
+++ routes/trips.js
@@ -7,11 +7,7 @@ router.get('/:departure/:arrival', (req, res) =>{
     
   Trip.find({departure : req.params.departure , arrival : req.params.arrival})
   .then(listTrip => {
-    const listDate = [];
-
-    for(i=0; i<listDate.length; i++)
       res.json({trip : listTrip});
-    })
   });
-
+})
 module.exports = router;
