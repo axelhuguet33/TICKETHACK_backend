72f6e1a4257fe5a8f47aa169e53c9cec29a0e471
Your name <Your email>
2024-04-16 14:11:46 +0200

    works en trips

--- routes/trips.js
+++ routes/trips.js
@@ -3,10 +3,13 @@ var router = express.Router();
 
 const Trip = require('../models/trips');
 
-router.get('/:departure/:arrival/:date', (req, res) =>{
+router.get('/:departure/:arrival', (req, res) =>{
     
-  Trip.find({departure : req.params.departure , arrival : req.params.arrival, date: req.params.date})
+  Trip.find({departure : req.params.departure , arrival : req.params.arrival})
   .then(listTrip => {
+    const listDate = [];
+
+    for(i=0; i<listDate.length; i++)
       res.json({trip : listTrip});
     })
   });
